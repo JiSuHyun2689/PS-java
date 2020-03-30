e7b8e800e7928b5377ff4089b21ac11dc8687a70
SuHyun <38772078+JiSuHyun2689@users.noreply.github.com>
2020-03-30 21:43:08 +0900

    2020-03-30 commit

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -3,7 +3,23 @@ package sort;
 public class InsertionSort {
 
 	public static void main(String[] args) {
+		
+		int i, j, temp;
+		int[] arr = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+		int length = arr.length;
+		
+		for(i = 0; i<length-1; i++) {
+			j = i ;
+			while(arr[j] > arr[j+1]) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				j--;
+			}
+		}
 
+		for(int target : arr)
+			System.out.print(target+ " ");
 	}
 
 }
