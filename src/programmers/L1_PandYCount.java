ecefac52968dfa9b7b06526a9e3c095c4b311247
SuHyun <38772078+JiSuHyun2689@users.noreply.github.com>
2020-04-08 23:25:07 +0900

    2020-04-08 commit

---
+++ src/programmers/L1_PandYCount.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class L1_PandYCount {
+
+	class Solution {
+		boolean solution(String s) {
+			// s.toUpperCase();
+			// s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+			int pC = 0, yC = 0;
+
+			for (int i = 0; i < s.length(); i++) {
+				String str = s.split("")[i];
+				if (str.equalsIgnoreCase("p"))
+					pC++;
+				else if (str.equalsIgnoreCase("y"))
+					yC++;
+			}
+
+			return pC == yC ? true : false;
+		}
+	}
+}
