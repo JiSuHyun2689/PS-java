01c5333eb2a16e9ae41e57f36ccdd8e353e29163
SuHyun <38772078+JiSuHyun2689@users.noreply.github.com>
2020-04-27 22:53:18 +0900

    2020-04-27 commit

---
+++ src/programmers/L2_SkillTree.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class L2_SkillTree {
+	public int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+		int[] arr = {};
+
+		for (int i = 0; i < skill_trees.length; i++) {
+			for (int j = 0; j < skill_trees[i].length() - 1; j++) {
+
+			}
+		}
+
+		return answer;
+	}
+}
---
+++ src/programmers/L2_Top.java
@@ -0,0 +1,52 @@
+package programmers;
+
+import java.util.Stack;
+
+class L2_Top {
+
+	class Top {
+		int idx;
+		int height;
+
+		public Top(int idx, int height) {
+			this.idx = idx;
+			this.height = height;
+		}
+	}
+
+	public int[] solution(int[] heights) {
+		int[] answer = new int[heights.length];
+		Stack<Top> stack = new Stack<Top>();
+
+		for (int i = 0; i < heights.length; i++) {
+
+			Top top = new Top(i + 1, heights[i]);
+			int idx = 0;
+
+			while (!stack.isEmpty()) {
+				Top before = stack.peek();
+
+				if (top.height < before.height) {
+					idx = before.idx;
+					break;
+				}
+				stack.pop();
+			}
+			stack.push(top);
+			answer[i] = idx;
+		}
+
+		return answer;
+	}
+
+}
+
+/*
+ * class Solution { public int[] solution(int[] heights) { int[] answer = new
+ * int[heights.length];
+ * 
+ * for(int i=heights.length-1; i>=0; i--){ for(int j=i-1; j>=0; j--){
+ * if(heights[i] < heights[j]){ answer[i] = j+1; break; } } }
+ * 
+ * return answer; } }
+ */
